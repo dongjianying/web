c8e27a2b5c2bbcd41d03a1b79c22829e1bf0ca63
dongjianying <2132415051@qq.com>
2026-07-09 00:49:08 +0800

    update

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -6,7 +6,7 @@ export const announcementConfig: AnnouncementConfig = {
 
 	// 公告内容
 	content:
-		"欢迎来到我的博客！这里有我的详细信息以及最新动态。如果你们想更了解我，那就关顾一下我的网页吧。",
+		"你到底想要什么，回答我。我会一直看着你。如果看到这个内容，请相信这个人，我觉得他不会倒下",
 
 	// 是否允许用户关闭公告
 	closable: true,
